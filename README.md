ee89a1b2dbe858982dd47b689978857734c7d4e9
Christian Steiger <christian.steiger@AcquiaMacBookPro-ChristianSteiger.fritz.box>
2017-11-13 16:37:50 +0100

    Add script to find orphan files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # unused-files
-Find all orphaned Drupal files
+Find all orphaned Drupal files with a simple PHP script.
+
+# How to run
+php findfiles.php /mnt/www/html/docroot/sites/default/files www.example.com
+
+Call the php file with two parameters:
+1. path to the files folder
+2. hostname of the site
---
+++ findfiles.php
@@ -0,0 +1,48 @@
+<?php
+
+    $files_path = $argv[1];
+    $uri = $argv[2];
+
+   	$result = "orphan_file_results.txt";
+
+    $command = 'cd '.$files_path. ' && drush sqlq "SELECT uri FROM file_managed" @uri';
+    $command = strtr($command, [
+      '@uri' => '--uri='.$uri,
+    ]);
+    $filesManaged = explode("\n",shell_exec($command));
+
+    $command = "find @location -type f -follow -print | grep -v -E '@excludes' | sort -nr";
+    
+    $command = strtr($command, [
+      '@location' => $files_path,
+      '@excludes' => '/js/js_|/css/css_|/php/twig/|/styles/',
+    ]);
+	
+	$filesDisk = explode("\n",shell_exec($command));
+    $comp = $comp1 = array();
+    foreach ($filesDisk as $file) {
+      $comp[] = str_replace($files_path, "", $file);
+    }
+    foreach ($filesManaged as $value) {
+      if(strpos($value, 'public://') === 0 || strpos($value, 'private://') === 0){
+              $value = str_replace('private://', "", $value);
+              $value = str_replace('public://', "", $value);
+              $comp1[] = $value;
+      }
+    }
+    $orphan = array_filter(array_diff ($comp, $comp1));
+    $fp = fopen($result, 'w');
+    foreach ($orphan as $key => $value) {
+    	fwrite($fp, $value . "\n");
+    }
+    fclose($fp);
+
+    $message = "We found @count orphan file(s), check out the details from @results " . "\n";
+	$message = strtr($message, [
+      '@count' => sizeof($orphan),
+      '@results' => dirname(__FILE__) . "/" . $result,
+    ]);
+
+    echo $message;
+
+    return;
